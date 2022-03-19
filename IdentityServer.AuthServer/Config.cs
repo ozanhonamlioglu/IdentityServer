40c60d78a73aeb58086dda45c8f316ae62b54c83
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-03-19 21:59:55 +0300

    Client credentials are complete

--- IdentityServer.AuthServer/Config.cs
+++ IdentityServer.AuthServer/Config.cs
@@ -9,7 +9,7 @@ namespace IdentityServer.AuthServer
         {
             return new[]
             {
-                new ApiResource("resource_api1") { Scopes = { "api1.read", "api1.write", "api1.update" } },
+                new ApiResource("resource_api1") { Scopes = { "api1.read", "api1.write", "api1.update" }, ApiSecrets = {new Secret("secretapi".Sha256())} },
                 new ApiResource("resource_api2") { Scopes = { "api2.read", "api2.write", "api2.update" } }
             };
         }
