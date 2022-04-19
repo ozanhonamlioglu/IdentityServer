f07a33d6004876b7bda7acafa2ab82cce8081561
ozan <ozanhonamlioglu@hotmail.com>
2022-04-19 10:22:37 +0300

    Securing client 1

--- IdentityServer.AuthServer/Config.cs
+++ IdentityServer.AuthServer/Config.cs
@@ -1,25 +1,26 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace IdentityServer.AuthServer
 {
-    public static class Config
+  public static class Config
+  {
+    public static IEnumerable<ApiResource> GetApiResources()
     {
-        public static IEnumerable<ApiResource> GetApiResources()
-        {
-            return new[]
-            {
+      return new[]
+      {
                 new ApiResource("resource_api1") { Scopes = { "api1.read", "api1.write", "api1.update" }, ApiSecrets = {new Secret("secretapi".Sha256())} },
                 new ApiResource("resource_api2") { Scopes = { "api2.read", "api2.write", "api2.update" } }
             };
-        }
+    }
 
-        public static IEnumerable<ApiScope> GetApiScopes()
-        {
-            return new[]
-            {
+    public static IEnumerable<ApiScope> GetApiScopes()
+    {
+      return new[]
+      {
                 new ApiScope("api1.read", "Read for API 1"),
                 new ApiScope("api1.write", "Read for API 1"),
                 new ApiScope("api1.update", "Read for API 1"),
@@ -27,12 +28,12 @@ namespace IdentityServer.AuthServer
                 new ApiScope("api2.write", "Read for API 2"),
                 new ApiScope("api2.update", "Read for API 2")
             };
-        }
+    }
 
-        public static IEnumerable<Client> GetClients()
-        {
-            return new[]
-            {
+    public static IEnumerable<Client> GetClients()
+    {
+      return new[]
+      {
                 new Client()
                 {
                     ClientId = "Client1",
@@ -48,22 +49,35 @@ namespace IdentityServer.AuthServer
                     ClientSecrets = new[] { new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = new[] { "api1.read", "api1.update", "api2.write", "api2.update" }
+                },
+                new Client()
+                {
+                    ClientId = "Client1-Mvc",
+                    ClientName = "Client1 mvc api",
+                    RequirePkce=false,
+                    ClientSecrets = new[] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new[] { "https://localhost:5002/signin-oidc" },
+                    AllowedScopes = new[] {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
-        }
+    }
 
-        public static IEnumerable<IdentityResource> GetIdentityResources()
-        {
-            return new IdentityResource[]
-            {
+    public static IEnumerable<IdentityResource> GetIdentityResources()
+    {
+      return new IdentityResource[]
+      {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile()
-            };
-        }
-        public static IEnumerable<TestUser> GetTestUsers()
-        {
-            return new[]
-            {
+      };
+    }
+    public static IEnumerable<TestUser> GetTestUsers()
+    {
+      return new[]
+      {
                 new TestUser {
                     SubjectId = "1",
                     Username = "ozan",
@@ -85,6 +99,6 @@ namespace IdentityServer.AuthServer
                     }
                 }
             };
-        }
     }
+  }
 }
--- IdentityServer.Client1/Startup.cs
+++ IdentityServer.Client1/Startup.cs
@@ -11,47 +11,62 @@ using System.Threading.Tasks;
 
 namespace IdentityServer.Client1
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthentication(opts =>
+      {
+        opts.DefaultScheme = "Cookies";
+        opts.DefaultChallengeScheme = "oidc";
+      }).AddCookie("Cookies")
+      .AddOpenIdConnect("oidc", opts =>
+      {
+        opts.SignInScheme = "Cookies";
+        opts.Authority = "https://localhost:5001";
+        opts.ClientId = "Client1-Mvc";
+        opts.ClientSecret = "secret";
+        opts.ResponseType = "code id_token";
+      });
+
+      services.AddControllersWithViews();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
 }
